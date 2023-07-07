75dbb38d264bcf5ca0e3219b50e4d1db6caf525a
ken <redcool007@qq.com>
2023-07-07 15:37:24 +0800

    v2.1.18.4 update PowerVFXMinVersionChecker add Version switch

--- PowerVFX/Scripts/Tools/PowerVFXMinVersionChecker.cs
+++ PowerVFX/Scripts/Tools/PowerVFXMinVersionChecker.cs
@@ -1,48 +1,72 @@
 #if UNITY_EDITOR
 using PowerUtilities;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEditor;
 using UnityEngine;
 
 public class PowerVFXMinVersionChecker
 {
-    static HashSet<string> keywordSet = new HashSet<string>{
-            "PBR_LIGHTING",
-			// "_RECEIVE_SHADOWS_ON",
-			"_ADDITIONAL_LIGHT_SHADOWS_SOFT",
-            "VERTEX_WAVE_ON",
-            "FRESNEL_ON",
-            //"ALPHA_TEST",
-            "DISTORTION_ON",
-            "DISSOLVE_ON",
-            "OFFSET_ON",
-            "ENV_REFLECT_ON",
-            "ENV_REFRACTION_ON",
-            "MATCAP_ON",
-            "MATCAP_ROTATE_ON",
-            "DEPTH_FADING_ON",
-            "DOUBLE_EFFECT_ON",
-            "_OFFSET_BLEND_REPLACE_MODE",
-            "SHEET_ANIM_BLEND_ON"
+    /// <summary>
+    /// MIN_VERSION, this keywords not to be used
+    /// </summary>
+    static readonly string[] keywords = new []
+    {
+        "PBR_LIGHTING",
+		// "_RECEIVE_SHADOWS_ON",
+		"_ADDITIONAL_LIGHT_SHADOWS_SOFT",
+        "VERTEX_WAVE_ON",
+        "FRESNEL_ON",
+        //"ALPHA_TEST",
+        //"DISTORTION_ON",
+        //"DISSOLVE_ON",
+        "OFFSET_ON",
+        "ENV_REFLECT_ON",
+        "ENV_REFRACTION_ON",
+        "MATCAP_ON",
+        "MATCAP_ROTATE_ON",
+        "DEPTH_FADING_ON",
+        "DOUBLE_EFFECT_ON",
+        "_OFFSET_BLEND_REPLACE_MODE",
+        "SHEET_ANIM_BLEND_ON"
     };
 
-    static bool IsKeywordsValid(string[] keywords)
+    /// <summary>
+    /// MIN_VERSION this properties not to be used
+    /// </summary>
+    static readonly string[] props = new[]
     {
-        foreach (var item in keywords)
+        "_MainTexOffset_CustomData_On",
+        "_MainTexMaskOffsetCustomDataOn",
+        // dissvole
+        "_PixelDissolveOn",
+        "_DissolveEdgeOn",
+        // distortion
+        "_DistortionRadialUVOn",
+    };
+
+    static bool IsKeywordsValid(string[] materialKeywords)
+        => IsValid(materialKeywords, materialKeyword => keywords.Contains(materialKeyword));
+
+    private static bool IsPropValid(Material m) => 
+        IsValid(props, propName => m.GetFloat(propName) != 0);
+    
+
+    static bool IsValid(IEnumerable<string> keys,Func<string,bool> onCheck)
+    {
+        foreach (var name in keys)
         {
-            if (keywordSet.Contains(item))
-                return false;
+            if(onCheck(name)) return false;
         }
         return true;
     }
 
     static bool IsValidMinVersion(Material m)
     {
-        var isPropValid = (m.GetFloat("_MainTexOffset_CustomData_On") == 0 && m.GetFloat("_MainTexMaskOffsetCustomDataOn") == 0);
-
-        return isPropValid && IsKeywordsValid(m.shaderKeywords);
+        return IsPropValid(m) && IsKeywordsValid(m.shaderKeywords);
     }
 
     [MenuItem("PowerUtilities/PowerVFX/Check MinVersion (selected folder)")]
